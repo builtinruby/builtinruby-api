29380823ef94cc3c58bf551d52f33fb9395bcc86
Daniel Vinciguerra <daniel.vinciguerra@bivee.com.br>
2020-09-29 09:07:40 -0300

    feat: add test to create post by endpoint

--- app.rb
+++ app.rb
@@ -1,6 +1,48 @@
+require 'date'
 require 'sinatra'
+require 'octokit'
 require 'sinatra/json'
+require 'securerandom'
+
+OCTOKIT_TOKEN = ENV['OCTOKIT_TOKEN']
+
+configure do
+  enable(:logging)
+
+  set :repository, ENV['BUILTINRUBY_REPOSITORY']
+
+  set :branch, ENV['BUILTINRUBY_BRANCH']
+end
+
+helpers do
+  def github
+    @github ||= Octokit::Client.new(access_token: OCTOKIT_TOKEN)
+  end
+end
+
+module CreateJob
+  class << self
+    def call(_input)
+      id = SecureRandom.uuid
+
+      github.create_contents(
+        settings.repository
+        "path/to/#{Date.today.to_s}-#{id}file.md",
+        "[event] created new job `#{id} file`",
+        'The File Content',
+        branch: settings.branch
+      )
+
+      return { id: id }
+    end
+  end
+end
 
 get '/' do
   json message: 'Welcomme to BuiltinRuby.com'
+end
+
+post '/jobs' do
+  result = CreateJob.call
+  json id: result[:id], message: 'JOB_CREATED'
 end
